d65e6b3907b58bb7e71137048eb740384d18dbd6
Renan Neves <64873345+camilaffpacheco@users.noreply.github.com>
2023-03-28 21:18:45 -0300

    arrumando estilo da tabela

--- src/views/TabelaView.vue
+++ src/views/TabelaView.vue
@@ -12,13 +12,17 @@
   </header>
   <div class="fundo">
   <center>
+  <div class="titulo-listagem">LISTAGEM DE ARQUIVOS</div>
   <div class="card">
-    <DataTable :value="customers" paginator :rows="1" 
-    tableStyle="color: #1E599D; font-size: 17px; font-family: 'Roboto'; font-style: normal; font-weight: 400;">
+    <input type="text" class="form-control botaoBusca" placeholder="Buscar...">
+    <b-button class="btn-limpar">Limpar</b-button>
+    <DataTable :value="customers" paginator :rows="10"
+    tableStyle="color: #1E599D; font-size: 20px; font-family: 'Roboto'; font-style: normal; font-weight: 400; margin-left: 120px;
+    margin-top: 50px; border-collapse: separate; border-spacing: 8px;">
       <template #empty> Data not found </template>
-      <Column field="name" header="NOME DO ARQUIVO" style="min-width: 12rem" sortable ></Column>
-      <Column field="name" header="TIPO DO ARQUIVO" style="min-width: 12rem" sortable ></Column>
-      <Column field="name" header="TAMANHO DO ARQUIVO" style="min-width: 12rem" sortable ></Column>
+      <Column field="name" header="NOME DO ARQUIVO" sortable ></Column>
+      <Column field="name" header="TIPO DO ARQUIVO" sortable ></Column>
+      <Column field="name" header="TAMANHO DO ARQUIVO" sortable ></Column>
     </DataTable>
   </div>
   </center>
@@ -59,6 +63,54 @@
     width: 70%;
     margin-top: 50px;
     border: 3px solid #1E599D;
+  }
+
+  .botaoBusca {
+    margin-bottom: 30px;
+    background: #B1D4E0;
+    color: #1E599D;
+    border: 2px solid #1E599D;
+    width: 20%;
+    font-size: 20px; 
+    font-family: 'Roboto'; 
+    font-style: normal; 
+    font-weight: 400;
+  }
+
+  .botaoBusca::placeholder {
+    color: #1E599D;
+    font-size: 20px; 
+    font-family: 'Roboto'; 
+    font-style: normal; 
+    font-weight: 400;
+  }
+
+  .titulo-listagem {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 45px;
+    line-height: 70px;
+    text-align: center;
+    color: #1435A0;
+    margin-top: 50px;
+  }
+
+  .btn-limpar {
+    border: 2px solid #1E599D;
+    color: #1E599D;
+    font-size: 20px; 
+    font-family: 'Roboto'; 
+    font-style: normal; 
+    font-weight: 400;
+    width: 8%;
+    border-radius: 5px;
+    margin-left: 92%;
+    margin-top: -6%;
+    height: 45px;
+    align-items: center;
+    justify-content: center;
+    display: flex;
   }
 </style>
   
