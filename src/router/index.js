e90213555fd87bc6a075aa02afe4b2360bb539b8
Zaion Gomes <gomeszaion@gmail.com>
2023-04-09 01:27:48 -0300

    screen to post information connection drive

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TabelaDados from '../views/TabelaView.vue';
 import MenuArquivosView from '../views/MenuArquivos.vue';
+import ConfigView from '../views/ConfigView.vue';
 
 Vue.use(VueRouter)
 
@@ -11,6 +12,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/config',
+    name: 'config',
+    component: ConfigView
   },
   {
     path: '/tabela',
