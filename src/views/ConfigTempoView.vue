e31ddc2a017edfd00de755051fdc911e5b51fbe8
Orlandi-a11 <71853528+Orlandi-a11@users.noreply.github.com>
2023-05-04 20:03:40 -0300

    inserção da tela de configuração de tempo

---
+++ src/views/ConfigTempoView.vue
@@ -0,0 +1,174 @@
+<template>
+    <div class="fundo">
+      <div id="appView">
+        <nav class="navbar navbar-expand-lg color-navbar">
+          <div class="container-left">
+            <img src="../assets/logo.png" alt="" width="60" height="60" class="img"/>
+          </div>
+          <div class="titulo">
+            TECH NINJAS
+          </div>
+        </nav>
+        <div class="titulo-config">CONFIGURAÇÕES</div>
+        <div class="parametros-tempo">
+          <div class="button-container">
+          <button class="my-button">
+          <span class="button-text">CONFIGURAR</span>
+          </button>
+        </div>
+        </div>
+      </div>
+        <div>
+        </div>      
+        <P>  CONFIGURAÇÃO DE TEMPO (HORAS): </P><select>
+          <option value="">Selecione um horario</option>
+          <option value="01:00">01:00</option>
+          <option value="02:00">02:00</option>
+          <option value="03:00">03:00</option>
+          <option value="04:00">04:00</option>
+          <option value="05:00">05:00</option>
+          <option value="06:00">06:00</option>
+          <option value="07:00">07:00</option>
+          <option value="08:00">08:00</option>
+          <option value="09:00">09:00</option>
+          <option value="10:00">10:00</option>
+          <option value="11:00">11:00</option>
+          <option value="12:00">12:00</option>
+          <option value="13:00">13:00</option>
+          <option value="14:00">14:00</option>
+          <option value="15:00">15:00</option>
+          <option value="16:00">16:00</option>
+          <option value="17:00">17:00</option>
+          <option value="18:00">18:00</option>
+          <option value="19:00">19:00</option>
+          <option value="20:00">20:00</option>
+          <option value="21:00">21:00</option>
+          <option value="22:00">22:00</option>
+          <option value="23:00">23:00</option>
+          <option value="24:00">24:00</option>
+        </select>
+    <br><br>
+    <br><br>   
+    <P>  CONFIGURAÇÃO DE TEMPO (MINUTOS): </P> <select>
+          <option value="">Selecione os minutos</option>
+          <option value="00:05">00:05</option>
+          <option value="00:10">00:10</option>
+          <option value="00:15">00:15</option>
+          <option value="00:20">00:20</option>
+          <option value="00:25">00:25</option>
+          <option value="00:30">00:30</option>
+          <option value="00:35">00:35</option>
+          <option value="00:40">00:40</option>
+          <option value="00:45">00:45</option>
+          <option value="00:50">00:50</option>
+          <option value="00:55">00:55</option>
+          <option value="00:60">00:60</option>
+   
+    </select>
+    <br><br>
+    </div>
+  </template>
+  
+  <style scoped>
+
+
+
+p {
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 100;
+        font-size: 18px;
+        line-height: 50px;
+        color: #1435A0;
+        margin-top: 20px;
+        text-align: center;
+}
+  
+  .parametros-tempo{
+    font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 25px;
+        line-height: 50px;
+        text-align: center;
+        color: #1435A0;
+        margin-top: 50px;
+  }
+  
+ select {
+  font-family: 'Roboto';
+    display: block;
+    width: 800px;
+    height: 50px;
+    background-color: white;
+    color: #1435A0;
+    font-size: 20px;
+    border: 2px solid;
+    border-radius: 12px;
+    padding: 12px 12px;
+    text-align: center;
+   margin: 0 auto;
+}
+
+option {
+  font-family: 'Roboto';
+  color: black;
+  font-size: 16px;
+
+}
+
+.button-container {
+  position: fixed;
+  top: 88%;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #1E599D;
+  color: #B1D4E0;
+  border-radius: 40px;
+  font-size: 20px;
+  font-weight: bold;
+  height: 8vh;
+  border: 2px solid #1E599D;
+  border-radius: 17px;
+  padding: 8px 10px;
+  font-size: 26px;
+  width: 300px;
+  text-align: center;
+}
+ .titulo-config {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 45px;
+  line-height: 70px;
+  text-align: center;
+  color: #1435A0;
+  margin-top: 50px;        
+    }
+
+  
+  .color-navbar {
+  background-color:#1E599D;
+    }
+  
+      .titulo {
+          font-family: 'Roboto';
+          font-style: normal;
+          font-weight: 600;
+          font-size: 38px;
+          line-height: 75px;
+          color: #B1D4E0;  
+          margin-left: 10px;
+      }
+  
+      .img {
+          margin-left: 10px;
+      }
+  
+      .fundo {
+          position: absolute;
+          background:  #B1D4E0;
+          height: 100%;
+          width: 100%;
+      }
+  </style>
