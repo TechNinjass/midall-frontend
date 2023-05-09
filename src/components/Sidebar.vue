ccc39d3d217e0a7a62295fcb102eaf0f19bd0316
Borgarelli <borgarellitavares@outlook.com>
2023-05-08 22:34:13 -0300

    Sidebar com link do dash

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,42 +1,34 @@
 <template>
-  <div
-    class="sidebar fixed group overflow-hidden left-0 top-0 z-10 h-screen w-[calc(3.73rem)] bg-blue-500 lg:hover:w-56 hover:w-44"
-  >
+  <div class="sidebar fixed group overflow-hidden left-0 top-0 z-10 h-screen w-[calc(3.73rem)] bg-blue-500 lg:hover:w-56 hover:w-44">
     <div class="h-full flex flex-col justify-between">
       <div>
-        <div class="mt-4 h-16 flex items-center">
-          <a href="" class="block w-max px-2.5">
+        <div class="mt-3 h-13 flex items-center">
+          <a href="" class="block w-max px-2.0 flex items-center">
             <img src="@/assets/logo.png" alt="" width="60" height="60" class="img" />
-            <span class="button-text w-max flex items-center p-2 gap-4 rounded-full text-sm lg:text-lg">TECH NINJAS</span>
+            <span class="button-text">TECH NINJAS</span>
           </a>
         </div>
         <div class="mt-10">
           <div class="px-1 -ml-px font-medium tracking-wide">
             <ul class="w-max space-y-4 group hover:w-full">
               <li>
-                <div class="button-container">
-                  <router-link to="Configuracoes">
-                    <button class="my-button h-5 w-5 ml-2 mr-2 lg:mr-4">
-                      <img src="@/assets/config.png" alt="listagem" class="button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
-                      <span class="button-text w-max flex items-center p-2 gap-4 rounded-full text-sm lg:text-lg">Configurações</span>
-                    </button>
+                <div class="button-container flex items-center">
+                  <router-link to="Configuracoes" class="flex items-center">
+                    <img src="@/assets/config.png" alt="listagem" class="button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
+                    <span class="button-text">Configurações</span>
                   </router-link>
                 </div>
               </li>
               <li>
-                <router-link to="MenuArquivos">
-                  <button class="my-button h-5 w-5 ml-2 mr-2 lg:mr-4">
-                    <img src="@/assets/arquivo.png" alt="upload" class="button-icon button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
-                    <span class="button-text w-max flex items-center p-2 gap-4 rounded-full text-sm lg:text-lg">Arquivos</span>
-                  </button>
+                <router-link to="MenuArquivos" class="flex items-center">
+                  <img src="@/assets/arquivo.png" alt="upload" class="button-icon button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
+                  <span class="button-text">Arquivos</span>
                 </router-link>
               </li>
               <li>
-                <a href="https://lookerstudio.google.com/reporting/981c0a4c-e39d-401b-ba8c-902dd78a3f66/page/BmrMD" target="_blank" rel="noopener noreferrer">
-                  <button class="my-button h-5 w-5 ml-2 mr-2 lg:mr-4">
-                    <img src="@/assets/dashboard.png" alt="dashboard" class="button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
-                    <span class="button-text w-max flex items-center p-2 gap-4 rounded-full text-sm lg:text-lg">Dashboard</span>
-                  </button>
+                <a href="https://lookerstudio.google.com/reporting/981c0a4c-e39d-401b-ba8c-902dd78a3f66/page/BmrMD" target="_blank" rel="noopener noreferrer" class="flex items-center">
+                  <img src="@/assets/dashboard.png" alt="dashboard" class="button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
+                  <span class="button-text">Dashboard</span>
                 </a>
               </li>
             </ul>
@@ -47,13 +39,38 @@
   </div>
 </template>
 
-<!-- " -->
-
 <script>
 export default {
   name: 'NavigationBar',
   data() {
     return {};
+  },
+  mounted() {
+    const sidebar = document.querySelector('.sidebar');
+    const images = sidebar.querySelectorAll('.button-icon');
+    const texts = sidebar.querySelectorAll('.button-text');
+
+    sidebar.addEventListener('mouseenter', () => {
+      texts.forEach((text) => {
+        text.style.display = 'inline';
+      });
+    });
+
+    sidebar.addEventListener('mouseleave', () => {
+      texts.forEach((text) => {
+        text.style.display = 'none';
+      });
+    });
+
+    images.forEach((image) => {
+      image.addEventListener('mouseenter', () => {
+        image.nextElementSibling.style.display = 'inline';
+      });
+
+      image.addEventListener('mouseleave', () => {
+        image.nextElementSibling.style.display = 'none';
+      });
+    });
   },
 };
 </script>
@@ -61,6 +78,12 @@ export default {
 <style scoped>
 .sidebar {
   transition: all 0.4s ease-in-out;
+}
+
+.button-text {
+  margin-left: 4px;
+  font-family: 'Roboto';
+  font-style: normal;  
 }
 
 span {
