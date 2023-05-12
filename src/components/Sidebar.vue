2a0ee1241a2a12c5ef908a65b36a2e5aa0d85634
Borgarelli <borgarellitavares@outlook.com>
2023-05-12 20:01:29 -0300

    sidebar finalizada

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -14,21 +14,35 @@
               <li>
                 <div class="button-container flex items-center">
                   <router-link to="Configuracoes" class="flex items-center">
-                    <img src="@/assets/config.png" alt="listagem" class="button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
+                    <img src="@/assets/cloud.png" alt="cloud" class="button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
+                    <span class="button-text"><b>CLOUD CONFIG</b></span>
+                  </router-link>
+                </div>
+              </li>
+              <li>
+                <div class="button-container flex items-center">
+                  <router-link to="Configuracoes" class="flex items-center">
+                    <img src="@/assets/configClaro.png" alt="listagem" class="button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
                     <span class="button-text"><b>CONFIGURAÇÕES</b></span>
                   </router-link>
                 </div>
               </li>
               <li>
                 <router-link to="MenuArquivos" class="flex items-center">
-                  <img src="@/assets/arquivo.png" alt="upload" class="button-icon button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
+                  <img src="@/assets/pasta.png" alt="upload" class="button-icon button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
                   <span class="button-text"><b>ARQUIVOS</b></span>
                 </router-link>
               </li>
               <li>
                 <a href="https://lookerstudio.google.com/reporting/981c0a4c-e39d-401b-ba8c-902dd78a3f66/page/BmrMD" target="_blank" rel="noopener noreferrer" class="flex items-center">
-                  <img src="@/assets/dashboard.png" alt="dashboard" class="button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
+                  <img src="@/assets/grafico-preditivo.png" alt="dashboard" class="button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
                   <span class="button-text"><b>DASHBOARD</b></span>
+                </a>
+              </li>
+              <li>
+                <a href="https://grafana-techninjas-bfe2ewdhhscha9bc.eus.grafana.azure.com/d/WJxn1ssVk/techninjas-grafana-1?orgId=1" target="_blank" rel="noopener noreferrer" class="flex items-center">
+                  <img src="@/assets/grafico-de-barras.png" alt="grafana" class="button-icon my-button h-5 w-5 ml-2 mr-2 lg:mr-4" />
+                  <span class="button-text"><b>GRAFANA</b></span>
                 </a>
               </li>
             </ul>
@@ -63,7 +77,8 @@ export default {
 .button-text {
   margin-left: 4px;
   font-family: 'Roboto';
-  font-style: normal;  
+  font-style: normal; 
+  color: #B1D4E0; 
 }
 
 span {
